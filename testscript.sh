99b47b1dae395c4dd535193305932961c9603dd6
haarora <haarora@redhat.com>
2020-02-23 20:37:01 +0530

    test commit

--- testscript.sh
+++ testscript.sh
@@ -2,5 +2,5 @@
 echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
-echo "new commit for testing phase 2 not working"
+echo "new commit for testing phase 2 not working keeping tryig"
 echo "this was run automagically"
